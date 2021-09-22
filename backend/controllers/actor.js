ed19ef3075eefe6ff1ffecf2e351cd347bddebe7
Reiq.Laptop <reiqvideos@gmail.com>
2021-09-22 05:39:20 -0400

    Actualizador de datos del actor en la API funcionando

--- backend/controllers/actor.js
+++ backend/controllers/actor.js
@@ -3,16 +3,16 @@ const ActorModel = require('../models/actor');
 const ActorController = {
     updatedActors: async (req, res) => {
         const Actors = await ActorModel.find().sort({ createdAt: -1 }).limit(4);
-        if(Actors.length > 0){
+        if (Actors.length > 0) {
             res.json(Actors);
         } else {
-            res.json({message: "No hay actores en la base de datos"})
+            res.json({ message: "No hay actores en la base de datos", error: true })
         }
     },
     createActor: async (req, res) => {
-        const {name, dateOfBirth, gender} = req.body;
-        const actorExist = await ActorModel.find({name: name});
-        if(actorExist.length === 0){
+        const { name, dateOfBirth, gender } = req.body;
+        const actorExist = await ActorModel.find({ name: name });
+        if (actorExist.length === 0) {
             const newActor = new ActorModel({
                 name: name,
                 dateOfBirth: dateOfBirth,
@@ -22,10 +22,30 @@ const ActorController = {
                 createdAt: Date.now()
             });
             await newActor.save();
-            res.json({message: "Actor creado con éxito!"});
+            res.json({ message: "Actor creado con éxito!", error: false });
         } else {
-            res.json({message: "Este actor ya fue registrado antes"});
+            res.json({ message: "Este actor ya fue registrado antes", error: true });
         }
+    },
+    updateActor: async (req, res) => {
+        const { name, dateOfBirth, gender } = req.body;
+        const idActor = req.params.id;
+        ActorModel.findById(idActor, async (err, actor) => {
+            if (actor) {
+                const newDataActor = {
+                    name: name,
+                    dateOfBirth: dateOfBirth,
+                    gender: gender,
+                    photo: null,
+                    films: [],
+                    createdAt: Date.now()
+                };
+                await ActorModel.findByIdAndUpdate(idActor, newDataActor);
+                res.json({ message: "Los datos del actor fueron actualizados con éxito!", error: false });
+            } else if (err) {
+                res.json({ message: "No se puede actualizar un actor que no existe", error: true });
+            }
+        });
     }
 }
 
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,8 +3,9 @@ const route = Router();
 const FilmController = require("../controllers/films");
 const ActorController = require("../controllers/actor");
 
-/* RUTAS DE LA API */
+/* RUTAS DE LA API PARA ACTORES */
 route.get('/api/actors/updated', ActorController.updatedActors);
 route.post('/api/actors/create', ActorController.createActor);
+route.put('/api/actors/update/:id', ActorController.updateActor);
 
 module.exports = route;
