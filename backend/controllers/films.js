076dfccab0daed48db5e89614d7a215102bbd2ec
Reiq.Laptop <reiqvideos@gmail.com>
2021-10-02 16:54:48 -0400

    API: Buscador de películas y obtener imágenes de las películas funcionando

--- backend/controllers/films.js
+++ backend/controllers/films.js
@@ -1,6 +1,32 @@
 const FilmModel = require('../models/films');
 
 const FilmController = {
+    /* BUSCADOR DE ACTORES */
+    searchFilm: async (req, res) => {
+        const { gender, title } = req.params;
+        if (gender === 'Todos') {
+            const films = await FilmModel.find({
+                title: { $regex: '.*' + title + '.*', $options: 'i' }
+            }).limit(4);
+            if (films.length === 0) {
+                res.json({ message: "No hay resultados de su búsqueda", error: true });
+            } else {
+                res.json({ message: "Resultados de la búsqueda", error: false, films: films });
+            }
+        } else {
+            const films = await FilmModel.find({
+                $and: [
+                    { title: { $regex: '.*' + title + '.*', $options: 'i' } },
+                    { gender: gender }
+                ]
+            }).limit(4);
+            if (films.length === 0) {
+                res.json({ message: "No hay resultados de su búsqueda", error: true });
+            } else {
+                res.json({ message: "Resultados de la búsqueda", error: false, films: films });
+            }
+        }
+    },
     /* CREACIÓN DE PELÍCULAS, ANTES DE CREARLO COMPRUEBA QUE EL NOMBRE NO ESTÉ REGISTRADO */
     createFilm: async (req, res) => {
         const { title, releaseDate, gender } = req.body;
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -13,18 +13,21 @@ const DeleteController = require("../controllers/helpers/delete");
 var photosActorsRoute = multipart({ uploadDir: './backend/public/img/actors' });
 var photosFilmsRoute = multipart({ uploadDir: './backend/public/img/films' });
 
-/* RUTAS DE LA API PARA PELÍCULAS */
-route.post('/api/films/create', FilmController.createFilm);
-
 /* RUTAS DE LA API PARA ACTORES */
 route.get('/api/actors/updated', ActorController.updatedActors);
-route.post('/api/actors/search/:gender/:name', ActorController.searchActor);
-route.post('/api/actors/create', ActorController.createActor);
 route.put('/api/actors/update/:id', ActorController.updateActor);
 
-/* HTTP - POST */
+/* HTTP - POST (CREAR) */
+route.post('/api/films/create', FilmController.createFilm);
+route.post('/api/actors/create', ActorController.createActor);
+
+/* HTTP - POST (IMAGENES) */
 route.post('/api/actors/upload-photo/:status/:id', photosActorsRoute, PostController.uploadImageActor);
 route.post('/api/films/upload-photo/:status/:id', photosFilmsRoute, PostController.uploadImageFilm);
+
+/* HTTP - GET (BUSCADOR) */
+route.post('/api/actors/search/:gender/:name', ActorController.searchActor);
+route.post('/api/films/search/:gender/:title', FilmController.searchFilm);
 
 /* HTTP - DELETE */
 route.delete('/api/actors/:id', DeleteController.deleteActor);
